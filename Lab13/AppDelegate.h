c26586cda5b97e4a1bcc40f26c40c6e64ff91df4
Kaiya Takahashi <yaikashi@icloud.com>
2022-09-14 09:31:35 -0700

    Initial Commit

---
+++ Lab13/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Lab13
+//
+//  Created by Kaiya Takahashi on 2022-09-14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
